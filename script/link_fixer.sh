c859f9785568a6e6cb49fd8c36210711b332e348
RoiArthurB <contact@arthurbrugiere.fr>
2019-05-08 11:43:54 +0700

    [Travis Script] link_fixer.sh

---
+++ script/link_fixer.sh
@@ -0,0 +1,19 @@
+##
+# Copyright (c) 2019-present, Arthur Brugiere, GAMA-Platform
+#
+# This source code is licensed under the GPL3 license found in the
+# LICENSE file in the root directory of this source tree.
+##
+
+# This script will correct images path in <files>.md.
+#
+# ex: <baseUrl>/[docs,wiki]/<file>
+#	prev: ![images/parent_species.png](resources/images/tutorials/parent_species.png)
+#	new : ![images/parent_species.png](../resources/images/tutorials/parent_species.png)
+
+#!/bin/sh
+
+for f in ./docs/*.md ; do
+	#echo $f
+	sed -r -i 's/!\[.*\]\(/&..\//g' "$f"
+done
