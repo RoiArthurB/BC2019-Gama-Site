ac14769508d66f9c8b9cbbcd336346858135ceae
RoiArthurB <contact@arthurbrugiere.fr>
2019-05-08 14:50:16 +0700

    [TRAVIS Fix] n°2 don't commit build -> NPM

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -44,7 +44,7 @@ const siteConfig = {
   //   baseUrl: '/test-site/',
 
   // Used for publishing and more
-  projectName: 'BC2019-Gama-Site',
+  projectName: 'BC20	19-Gama-Site',
   organizationName: 'RoiArthurB',
   // For top-level user or org sites, the organization is still the same.
   // e.g., for the https://JoelMarcey.github.io site, it would be set like...
