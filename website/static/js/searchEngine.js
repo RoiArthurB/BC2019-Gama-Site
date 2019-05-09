b51d888ef53fabe0f1bf3082fa87912b327d96b1
RoiArthurB <contact@arthurbrugiere.fr>
2019-05-10 02:28:41 +0700

    [Search] Better Fuse parameters

--- website/static/js/searchEngine.js
+++ website/static/js/searchEngine.js
@@ -7,14 +7,16 @@ var database, fuse, searchInput;
 
 var options = {
   shouldSort: true,
-  threshold: 0.6,
+  tokenize: true,
+  findAllMatches: true,
+  threshold: 0.4,
   location: 0,
   distance: 100,
   maxPatternLength: 32,
   minMatchCharLength: 1,
   keys: [
     "title",
-    "author.firstName"
+    "tag"
   ]
 };
 
