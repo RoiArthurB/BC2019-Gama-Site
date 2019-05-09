fdda8d297a6816ccb2714582b78a7d14c3f701bf
RoiArthurB <contact@arthurbrugiere.fr>
2019-05-09 10:38:37 +0700

    [UI Front] First release front page

--- website/pages/en/index.js
+++ website/pages/en/index.js
@@ -30,9 +30,12 @@ class HomeSplash extends React.Component {
       </div>
     );
 
+    const FlexContainer = props => ( <div className="section container"> {props.children} </div> );
+    const FlexItem = props => ( <div className="item">{props.children}</div> );
+
     const Logo = props => (
       <div className="projectLogo">
-        <img src={siteConfig.headerIcon} alt="Project Logo" />
+        <img src={props.src} alt="Project Logo" />
       </div>
     );
 
@@ -53,7 +56,7 @@ class HomeSplash extends React.Component {
 
     const Button = props => (
       <div className="pluginWrapper buttonWrapper">
-        <a className="button" href={props.href} target={props.target}>
+        <a className={props.className} href={props.href} target={props.target}>
           {props.children}
         </a>
       </div>
@@ -65,23 +68,42 @@ class HomeSplash extends React.Component {
       </div>
     );
 
+    return (
+      <SplashContainer>
+        <div className="inner">
+          <FlexContainer>
+            <FlexItem>
+              <Logo src={`${baseUrl}${siteConfig.headerIcon}`} />
+            </FlexItem>
+            <FlexItem>
+              <ProjectTitle siteConfig={siteConfig} />
+            </FlexItem>
+          </FlexContainer>
+          <PromoSection>
+            <Button href={docUrl('Home')} className="button button1">Discover</Button>
+            <Button href={docUrl('Tutorials')} className="button button2">Learn</Button>
+            <Button href={docUrl('Tutorials')} className="button button3">Masterize</Button>
+            <Button href={docUrl('Download')} className="button button4">Download</Button>
+          </PromoSection>
+          <ImgSoftware img_src={`${baseUrl}img/flipper.png`} />
+        </div>
+      </SplashContainer>
+    );
+  }
+}
+
+class Index extends React.Component {
+  render() {
+    const {config: siteConfig, language = ''} = this.props;
+    const {baseUrl} = siteConfig;
+
     const Design = props => (    
       <style dangerouslySetInnerHTML={{__html: `
-        .fixedHeaderContainer, footer{
-          display: none;
-        }
-
-        .navPusher{
-          padding: 0;
-        }
+        /*  One Screen  */
+        .fixedHeaderContainer, footer{ display: none; }
+        .navPusher{ padding: 0; }
 
         .homeSplashFade {
-            max-height: 100vh;
-            background: linear-gradient(#121020,#21233e);
-            overflow: hidden;
-        }
-        .homeSplashFade {
-            min-height: 700px;
             max-height: 100vh;
             background: linear-gradient(#121020,#21233e);
             position: relative;
@@ -94,36 +116,40 @@ class HomeSplash extends React.Component {
           width: 50%;
           margin: 1.5em auto 0 auto;
         }
+        .promoSection { margin: 3em; }
 
-        .promoSection {
-            margin: 3em;
+        .projectLogo {
+          position: inherit !important;
         }
+
+        /*  BUTTONS */
+        .button1 {
+          border: 1px solid #EEB64F;
+          color: #EEB64F;
+        }
+        .button1:hover{ background: #EEB64F; }
+        .button2 {
+          border: 1px solid #D2673B;
+          color: #D2673B;
+        }
+        .button2:hover{ background: #D2673B; }
+        .button3 {
+          border: 1px solid #3670A0;
+          color: #3670A0;
+        }
+        .button3:hover{ background: #3670A0; }
+        .button4:hover {
+          border: 1px solid #fff;
+          background: #fff;
+          color: black;
+        }
+        .button4{ background: #ddd; color: black; }
       `}} />
     );
 
     return (
-      <SplashContainer>
-        <div className="inner">
-          <ProjectTitle siteConfig={siteConfig} />
-          <PromoSection>
-            <Button href={docUrl('Home')}>Documentation</Button>
-            <Button href={docUrl('Tutorials')}>Tutorials</Button>
-          </PromoSection>
-          <ImgSoftware img_src={`${baseUrl}img/flipper.png`} />
-          <Design/>
-        </div>
-      </SplashContainer>
-    );
-  }
-}
-
-class Index extends React.Component {
-  render() {
-    const {config: siteConfig, language = ''} = this.props;
-    const {baseUrl} = siteConfig;
-
-    return (
       <div>
+        <Design/>
         <HomeSplash siteConfig={siteConfig} language={language} />
       </div>
     );
